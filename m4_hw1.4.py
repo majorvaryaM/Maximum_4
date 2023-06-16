d929e6f06f8b52c3897bb0a40bcdf785eb6a2a4c
majorvaryaM <majorvarya@gmail.com>
2023-06-16 15:27:01 +0500

    first_commit

---
+++ m4_hw1.4.py
@@ -0,0 +1,9 @@
+# Определитель палиндромов
+str_1 = input ('Введите фразу без пробелов с маленькой буквы: ')
+def Palindrom_finder(str_1):
+    str_2 = str_1[::-1]
+    if str_1 == str_2:
+        return True
+    else: 
+        return  False
+print (Palindrom_finder(str_1))
