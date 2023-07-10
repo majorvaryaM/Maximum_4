68480d85862b008f0d7d915f69dc456df450ad5e
majorvaryaM <majorvarya@gmail.com>
2023-07-10 11:17:26 +0500

    к уроку 4

---
+++ less_4/app_lesson_4/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+# подключаем объект для выполнения http-запросов
+from django.http import HttpResponse
+
+# Create your views here.
+def index (request):
+    return HttpResponse ('Домашка по 4 занятию')
