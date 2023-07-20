e464708d17de99f9df566c2a261b421bfe47bed2
majorvaryaM <majorvarya@gmail.com>
2023-07-20 12:43:27 +0500

    код с 5 зан

--- less_5/online_shop/app_online_shop/views.py
+++ less_5/online_shop/app_online_shop/views.py
@@ -5,5 +5,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 # Функция, отображающая файл index.html
-def index(request):
-    return render(request, 'index.html') 
+def index (request):
+    return render (request, 'index.html')
+
+# Функция, отображающая файл top_sellers.html
+def top_sellers (request):
+    return render (request, 'top-sellers.html')
