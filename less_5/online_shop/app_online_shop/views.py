668801732f2b8aa02870cacc48241b40471e9001
majorvaryaM <majorvarya@gmail.com>
2023-07-24 15:37:56 +0500

    домашка 5 занятие

--- less_5/online_shop/app_online_shop/views.py
+++ less_5/online_shop/app_online_shop/views.py
@@ -11,3 +11,19 @@ def index (request):
 # Функция, отображающая файл top_sellers.html
 def top_sellers (request):
     return render (request, 'top-sellers.html')
+
+# Функция, отображающая файл advertisement-post.html
+def advertisement_post (request):
+    return render (request, 'advertisement-post.html')
+
+# Функция, отображающая файл login.html
+def login (request):
+    return render (request, 'login.html')
+
+# Функция, отображающая файл register.html
+def register (request):
+    return render (request, 'register.html')
+
+# Функция, отображающая файл profile.html
+def profile (request):
+    return render (request, 'profile.html')
